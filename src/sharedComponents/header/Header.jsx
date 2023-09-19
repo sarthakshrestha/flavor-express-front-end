055b3b4b237ecd2f640095ca878d2d1c34469af7
sarthakshrestha <sarthakstha24@gmail.com>
2023-09-19 14:14:33 +0545

    Finalized Navbar.

--- src/sharedComponents/header/Header.jsx
+++ src/sharedComponents/header/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import "./Header.css";
 import logoImage from "./Logo2.png";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-regular-svg-icons";
+import {faCartShopping} from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
     return (
         <header>
             <nav className="navbar">
                 <div className="top">
-                    <img src={logoImage} alt="logo" className="logo" />
+                    <img src={logoImage} alt="logo" className="logo"/>
                     <div className="links">
                         <ul>
                             <li><NavLink to="/">Home</NavLink></li>
@@ -20,8 +23,15 @@ export default function Header() {
                     </div>
                 </div>
                 <div className="user-actions">
-                    <NavLink to="/account" className="action-button">Account</NavLink>
-                    <NavLink to="/cart" className="action-button">Cart</NavLink>
+                    <div className="accountButton">
+
+                        <NavLink to="/account" className="action-button">Account<FontAwesomeIcon className="accIcon"
+                                                                                                     icon={faUser}/></NavLink>
+
+                    </div>
+                    <div className="cartButton">
+                        <NavLink to="/cart" className="action-button">Cart<FontAwesomeIcon className="cartIcon" icon={faCartShopping} /></NavLink>
+                    </div>
                 </div>
             </nav>
         </header>

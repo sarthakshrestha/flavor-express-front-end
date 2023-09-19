fb2cbc6c1364f0893a74af316c1fb4f8d1c75b0b
sarthakshrestha <sarthakstha24@gmail.com>
2023-09-19 13:25:49 +0545

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import HomePage from "./Components/homePage/HomePage";
 import LoginPage from "./Components/loginPage/LoginPage";
 import AdminLoginPage from "./Components/loginPage/AdminLoginPage";
 import RestaurantsPage from "./Components/restaurantsPage/RestaurantsPage";
 import RegistrationPage from "./Components/registrationPage/RegistrationPage";
+import Header from "./sharedComponents/header/Header";
 
 
 function App() {
-  return (
-      <div className="appContainer">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/loginPage" element={<LoginPage />} />
-          <Route path="/adminLoginPage" element={<AdminLoginPage />} />
-          {/*<Route path="/restaurantLoginPage" element={<RestaurantLoginPage />} />*/}
-          <Route path="/registrationPage" element={<RegistrationPage />} />
-          <Route path="/restaurantsPage" element={<RestaurantsPage />} />
-          {/*<Route path="/restaurantAboutPage" element={<RestaurantAboutPage />} />*/}
-          {/*<Route path="/foodItemsPage" element={<FoodItemsPage />} />*/}
-          {/*<Route path="/cartPage" element={<CartPage />} />*/}
-          <Route path="*" element={<h1>PageNotFound! :(</h1>} />
-        </Routes>
-      </div>
-  );
+    return (
+        <div className="appContainer">
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/nav" element={<Header/>}/>
+                <Route path="/loginPage" element={<LoginPage/>}/>
+                <Route path="/adminLoginPage" element={<AdminLoginPage/>}/>
+                {/*<Route path="/restaurantLoginPage" element={<RestaurantLoginPage />} />*/}
+                <Route path="/registrationPage" element={<RegistrationPage/>}/>
+                <Route path="/restaurantsPage" element={<RestaurantsPage/>}/>
+
+                {/*<Route path="/restaurantAboutPage" element={<RestaurantAboutPage />} />*/}
+                {/*<Route path="/foodItemsPage" element={<FoodItemsPage />} />*/}
+                {/*<Route path="/cartPage" element={<CartPage />} />*/}
+                <Route path="*" element={<h1>PageNotFound! :(</h1>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/sharedComponents/header/Header.jsx
+++ src/sharedComponents/header/Header.jsx
@@ -1,5 +1,29 @@
 import React from "react";
-import reactDOM from "react-dom";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+import logoImage from "./Logo2.png";
 
-export default function Header(){
+export default function Header() {
+    return (
+        <header>
+            <nav className="navbar">
+                <div className="top">
+                    <img src={logoImage} alt="logo" className="logo" />
+                    <div className="links">
+                        <ul>
+                            <li><NavLink to="/">Home</NavLink></li>
+                            <li><NavLink to="/restaurants">Restaurants</NavLink></li>
+                            <li><NavLink to="/food-items">Food Items</NavLink></li>
+                            <li><NavLink to="/meal-plans">Meal Plans</NavLink></li>
+                            <li><NavLink to="/offers">Offers</NavLink></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="user-actions">
+                    <NavLink to="/account" className="action-button">Account</NavLink>
+                    <NavLink to="/cart" className="action-button">Cart</NavLink>
+                </div>
+            </nav>
+        </header>
+    );
 }

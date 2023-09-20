98a941958b36a25d76e6cbcb4832586ae2bc6ff4
sarthakshrestha <sarthakstha24@gmail.com>
2023-09-20 22:25:36 +0545

    Initialized User List of Dashboard

---
+++ src/Components/adminDashboard/usersDashboard/usersListDashboard.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import ReactDOM from "react-dom";
+import Sidebar from "../sideBar/sideBar";
+import React from "react";
+
+export default function userList(){
+    return(<>
+            <div className="s-view">
+                <Sidebar/>
+            </div>
+        </>
+    );
+}

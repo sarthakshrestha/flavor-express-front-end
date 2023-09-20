53481b4a411b2add26816ba1cd5d932f2037c28f
sarthakshrestha <sarthakstha24@gmail.com>
2023-09-21 03:51:06 +0545

    UI Changes

--- src/Components/cartPage/cart.jsx
+++ src/Components/cartPage/cart.jsx
@@ -5,9 +5,32 @@ import Footer from "../../sharedComponents/footer/Footer";
 import "./cartPage.css";
 
 export default function Cart() {
+
+    const cartItems = [
+        {
+            id: 1,
+            photo: 'coffee.png',
+            name: 'Food Item 1',
+            description: 'Delicious food item with a great taste.',
+            allergy: 'Contains Dairy Product',
+            quantity: 2,
+            price: 10.99,
+        },
+        {
+            id: 2,
+            photo: 'pizza.png',
+            name: 'Food Item 2',
+            description: 'Another tasty food item for your enjoyment.',
+            allergy: 'Contains Soy',
+            quantity: 3,
+            price: 8.49,
+        },
+    ];
+
     return (<>
             <Header/>
             <div className="cartHeading">
+                <h2>Welcome to</h2>
                 <h2>Your Cart</h2>
             </div>
             <div className="nutritional-breakdown">
@@ -34,9 +57,49 @@ export default function Cart() {
                 </div>
             </div>
             <div className="food-cart">
-
-
+                <table className="food-table">
+                    <thead>
+                    <tr className="first-row">
+                        <th>S.N</th>
+                        <th>Photo</th>
+                        <th>Name</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {cartItems.map((item, index) => (
+                        <tr className="table-row" key={item.id}>
+                            <td>{index + 1}</td>
+                            <td>
+                                <img
+                                    className="food-image"
+                                    src={require(`./images/${item.photo}`)}
+                                    alt={item.name}
+                                />
+                            </td>
+                            <td>
+                                <div className="cart-items"> {/* Updated class name */}
+                                    <h3>{item.name}</h3>
+                                    <p>{item.description}</p>
+                                    <p className="allergy">Allergies: {item.allergy}</p>
+                                    <br/>
+                                    <button className="remove-item">Remove Items</button>
+                                </div>
+                            </td>
+                            <td>{item.quantity}</td>
+                            <td className="price-td">${item.price.toFixed(2)}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+                <hr className="rounded"></hr>
             </div>
+        <div className="calculation">
+            <h1>Your Total: </h1>
+            <p>Note: 5% is for the delivery charge</p>
+        </div>
+
 
             <Footer/>
         </>)

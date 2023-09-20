14f05d75684dfcf1ef6138f80f5fd6c8637bd98d
sarthakshrestha <sarthakstha24@gmail.com>
2023-09-20 17:41:01 +0545

    Created Admin Sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Header from "./sharedComponents/header/Header";
 import FoodPage from "./Components/foodPage/foodPage";
 import RestaurantPage from "./Components/restaurantPage/restaurantPage";
 import Cart from "./Components/cartPage/cart";
-
+import AdminDashboard from "./Components/adminDashboard/adminDashboard";
 function App() {
     return (
         <div className="appContainer">
@@ -18,6 +18,7 @@ function App() {
                 <Route path="/nav" element={<Header/>}/>
                 <Route path="/loginPage" element={<LoginPage/>}/>
                 <Route path="/adminLoginPage" element={<AdminLoginPage/>}/>
+                <Route path="/admin" element={<AdminDashboard/>}/>
                 {/*<Route path="/restaurantLoginPage" element={<RestaurantLoginPage />} />*/}
                 <Route path="/registrationPage" element={<RegistrationPage/>}/>
                 <Route path="/foodPage" element={<FoodPage/>}/>
---
+++ src/Components/adminDashboard/adminDashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Sidebar from "./sideBar/sideBar";
+
+export default function adminDashboard(){
+    return(<>
+        <Sidebar/>
+
+    </>);
+}

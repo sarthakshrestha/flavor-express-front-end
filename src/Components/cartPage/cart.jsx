b6f8636ddf845653846826196c2cd301670777b8
sarthakshrestha <sarthakstha24@gmail.com>
2023-09-24 09:12:11 +0545

    Cart Refresh Bug

--- src/Components/cartPage/cart.jsx
+++ src/Components/cartPage/cart.jsx
@@ -1,153 +1,147 @@
 import React, { useState, useEffect } from "react";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Header from "../../sharedComponents/header/Header";
 import Footer from "../../sharedComponents/footer/Footer";
 import "./cartPage.css";
+import Cookies from "js-cookie";
 import { useCart } from "./cartContext";
+import { calculateTotalNutrition } from "./cartContext"; // Import calculateTotalNutrition from cartContext
+
+const PRICE_PREFIX = "Rs. ";
+const DELIVERY_CHARGE_PERCENTAGE = 5;
+
+const parsePrice = (price) =>
+  parseFloat(price.replace(PRICE_PREFIX, "").replace(",", ""));
+const calculateItemTotalPrice = (item) =>
+  parsePrice(item.price) * item.quantity;
+const calculateDeliveryCharge = (totalPrice) =>
+  (totalPrice * DELIVERY_CHARGE_PERCENTAGE) / 100;
 
 export default function Cart() {
-    const [promoCode, setPromoCode] = useState("");
-    const { cartItems, removeFromCart, decrementQuantity, incrementQuantity, addToCart } = useCart(); 
+  const [promoCode, setPromoCode] = useState("");
+  const { cartItems, removeFromCart, decrementQuantity, incrementQuantity, calculateTotalPrice } = useCart();
 
-    const [totalNutrition, setTotalNutrition] = useState({
-        calories: 0,
-        protein: 0,
-        carbohydrates: 0,
-        fat: 0,
-    });
+  const [totalNutrition, setTotalNutrition] = useState({
+    calories: 0,
+    protein: 0,
+    carbohydrates: 0,
+    fat: 0,
+  });
 
-    useEffect(() => {
-        // Calculate the total nutritional content when the cartItems change
-        const newTotalNutrition = cartItems.reduce(
-            (acc, item) => {
-                acc.calories += item.nutrition.calories * item.quantity;
-                acc.protein += item.nutrition.protein * item.quantity;
-                acc.carbohydrates += item.nutrition.carbohydrates * item.quantity;
-                acc.fat += item.nutrition.fat * item.quantity;
-                return acc;
-            },
-            {
-                calories: 0,
-                protein: 0,
-                carbohydrates: 0,
-                fat: 0,
-            }
-        );
+  useEffect(() => {
+    // Update local storage with the current cart items
+    Cookies.set("cartItems", JSON.stringify(cartItems));
+    
+    // Calculate and set the total nutrition when cart items change
+    const nutrition = calculateTotalNutrition(cartItems); // Calculate the nutrition
+    setTotalNutrition(nutrition); // Set the total nutrition
+  }, [cartItems]);
 
-    const handlePromoCodeChange = (event) => {
-        setPromoCode(event.target.value);
-    };
+  const handlePromoCodeChange = (event) => {
+    setPromoCode(event.target.value);
+    // Apply and validate promo code logic here
+  };
 
-    setTotalNutrition(newTotalNutrition);
-    }, [cartItems]);
-
-    const handlePromoCodeChange = (event) => {
-        setPromoCode(event.target.value);
-    };
-
-    // Changes
-    return (
-        <>
-            <Header />
-            <div className="cartHeading">
-                <h2>Welcome to</h2>
-                <h2>Your Cart</h2>
-            </div>
-            <div className="nutritional-breakdown">
-                <h1 className="n-heading">Nutritional Breakdown</h1>
-                <div className="nutrient">
-                    <span className="nutrient-label">Calories:</span>
-                    <span className="nutrient-value">{totalNutrition.calories} cal</span>
-                </div>
-                <div className="nutrient">
-                    <span className="nutrient-label">Protein:</span>
-                    <span className="nutrient-value">{totalNutrition.protein} g</span>
-                </div>
-                <div className="nutrient">
-                    <span className="nutrient-label">Carbohydrates:</span>
-                    <span className="nutrient-value">{totalNutrition.carbohydrates} g</span>
-                </div>
-                <div className="nutrient">
-                    <span className="nutrient-label">Fat:</span>
-                    <span className="nutrient-value">{totalNutrition.fat} g</span>
-                </div>
-            </div>
-            <div className="food-cart">
-                <table className="food-table">
-                    <thead>
-                    <tr className="first-row">
-                        <th>S.N</th>
-                        <th>Photo</th>
-                        <th>Name</th>
-                        <th>Quantity</th>
-                        <th>Price</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {cartItems.map((item, index) => (
-                        <tr className="table-row" key={item.id}>
-                            <td>{index + 1}</td>
-                            <td>
-                                <img
-                                    className="food-image"
-                                    src={require(`../../Components/foodPage/images/${item.image}`)}
-                                    alt={item.name}
-                                />
-                            </td>
-                            <td>
-                                <div className="cart-items">
-                                    <h3>{item.name}</h3>
-                                    <p>{item.description}</p>
-                                    <p className="allergy">Allergies: {item.allergies}</p>
-                                    <br />
-                                    <button
-                                        className="remove-item"
-                                        onClick={() => removeFromCart(item.id)} // Use removeFromCart to remove the item
-                                    >
-                                        Remove Item
-                                    </button>
-                                </div>
-                            </td>
-                            <td>
-                                <button
-                                    className="quantity-button"
-                                    onClick={() => decrementQuantity(item.id)} // Use decrementQuantity to decrease the quantity
-                                >
-                                    -
-                                </button>
-                                <span className="quantity">{item.quantity}</span>
-                                <button
-                                    className="quantity-button"
-                                    onClick={() => incrementQuantity(item.id)} // Use incrementQuantity to increase the quantity
-                                >
-                                    +
-                                </button>
-                            </td>
-                            <td className="price-td">{item.price}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-                <hr className="rounded"></hr>
-            </div>
-            <div className="calculation">
-                <h1>Your Total: </h1>
-                <p>Note: 5% is for the delivery charge</p>
-            </div>
-            <hr className="rounded-bar"></hr>
-            <div className="promo-code-box">
-                <input
-                    type="text"
-                    placeholder="Enter Your Promo Code"
-                    value={promoCode}
-                    onChange={handlePromoCodeChange}
-                    className="promo-code-input"
-                />
-                <button className="apply-promo-button">Apply</button>
-            </div>
-            <Footer />
-
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div className="cartHeading">
+        <h2>Welcome to</h2>
+        <h2>Your Cart</h2>
+      </div>
+      <div className="nutritional-breakdown">
+        <h1 className="n-heading">Nutritional Breakdown</h1>
+        <div className="nutrient">
+          <span className="nutrient-label">Calories:</span>
+          <span className="nutrient-value">{totalNutrition.calories} cal</span>
+        </div>
+        <div className="nutrient">
+          <span className="nutrient-label">Protein:</span>
+          <span className="nutrient-value">{totalNutrition.protein} g</span>
+        </div>
+        <div className="nutrient">
+          <span className="nutrient-label">Carbohydrates:</span>
+          <span className="nutrient-value">
+            {totalNutrition.carbohydrates} g
+          </span>
+        </div>
+        <div className="nutrient">
+          <span className="nutrient-label">Fat:</span>
+          <span className="nutrient-value">{totalNutrition.fat} g</span>
+        </div>
+      </div>
+      <div className="food-cart">
+        <table className="food-table">
+          <thead>
+            <tr className="first-row">
+              <th>S.N</th>
+              <th>Photo</th>
+              <th>Name</th>
+              <th>Quantity</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map((item, index) => (
+              <tr className="table-row" key={item.id}>
+                <td>{index + 1}</td>
+                <td>
+                  <img
+                    className="food-image"
+                    src={require(`../../Components/foodPage/images/${item.image}`)}
+                    alt={item.name}
+                  />
+                </td>
+                <td>
+                  <div className="cart-items">
+                    <h3>{item.name}</h3>
+                    <p>{item.description}</p>
+                    <p className="allergy">Allergies: {item.allergies}</p>
+                    <br />
+                    <button
+                      className="remove-item"
+                      onClick={() => removeFromCart(item.id)}
+                    >
+                      Remove Item
+                    </button>
+                  </div>
+                </td>
+                <td>
+                  <button
+                    className="quantity-button"
+                    onClick={() => incrementQuantity(item.id)}
+                  >
+                    +
+                  </button>
+                  <span className="quantity">{item.quantity}</span>
+                  <button
+                    className="quantity-button"
+                    onClick={() => decrementQuantity(item.id)}
+                  >
+                    -
+                  </button>
+                </td>
+                <td className="price-td">{calculateItemTotalPrice(item)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <hr className="rounded" />
+      </div>
+      <div className="calculation">
+        <h1>Your Total: {calculateTotalPrice()}</h1>
+        <p>Note: 5% is for the delivery charge</p>
+      </div>
+      <div className="promo-code-box">
+        <input
+          type="text"
+          placeholder="Enter Your Promo Code"
+          value={promoCode}
+          onChange={handlePromoCodeChange}
+          className="promo-code-input"
+        />
+        <button className="apply-promo-button">Apply</button>
+      </div>
+      <Footer />
+    </>
+  );
 }
--- src/Components/cartPage/cartContext.jsx
+++ src/Components/cartPage/cartContext.jsx
@@ -1,73 +1,113 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 // Create a context for the shopping cart
 const CartContext = createContext();
 
+export const calculateTotalNutrition = (cartItems) => {
+  return cartItems.reduce(
+    (acc, item) => {
+      acc.calories += item.nutrition.calories * item.quantity;
+      acc.protein += item.nutrition.protein * item.quantity;
+      acc.carbohydrates += item.nutrition.carbohydrates * item.quantity;
+      acc.fat += item.nutrition.fat * item.quantity;
+      return acc;
+    },
+    {
+      calories: 0,
+      protein: 0,
+      carbohydrates: 0,
+      fat: 0,
+    }
+  );
+};
+
 // Create a custom hook to access the cart context
 export function useCart() {
-    return useContext(CartContext);
+  return useContext(CartContext);
 }
 
 // Create a CartProvider component to wrap your app
 export function CartProvider({ children }) {
-    const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
-    // Function to add an item to the cart
-    const addToCart = (product) => {
-        setCartItems((prevCartItems) => [...prevCartItems, product]);
-    };
+  // Function to add an item to the cart
+  const addToCart = (product) => {
+    setCartItems((prevCartItems) => [...prevCartItems, product]);
+  };
 
-    // Function to remove an item from the cart
-    const removeFromCart = (productId) => {
-        setCartItems((prevCartItems) =>
-            prevCartItems.filter((item) => item.id !== productId)
-        );
-    };
-
-    // Function to clear the entire cart
-    const clearCart = () => {
-        setCartItems([]);
-    };
-
-    // Calculate the total price of items in the cart
-    const calculateTotalPrice = () => {
-        return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-    };
-
-    // Function to increment the quantity of an item in the cart
-    const incrementQuantity = (productId) => {
-        setCartItems((prevCartItems) =>
-            prevCartItems.map((item) =>
-                item.id === productId ? { ...item, quantity: item.quantity + 1 } : item
-            )
-        );
-    };
-
-    // Function to decrement the quantity of an item in the cart
-    const decrementQuantity = (productId) => {
-        setCartItems((prevCartItems) =>
-            prevCartItems.map((item) =>
-                item.id === productId && item.quantity > 1
-                    ? { ...item, quantity: item.quantity - 1 }
-                    : item
-            )
-        );
-    };
-
-    // Create the value object to provide to the context
-    const value = {
-        cartItems,
-        addToCart,
-        removeFromCart,
-        clearCart,
-        calculateTotalPrice,
-        incrementQuantity,
-        decrementQuantity,
-    };
-
-    return (
-        <CartContext.Provider value={value}>
-            {children}
-        </CartContext.Provider>
+  // Function to remove an item from the cart
+  const removeFromCart = (productId) => {
+    setCartItems((prevCartItems) =>
+      prevCartItems.filter((item) => item.id !== productId)
     );
+  };
+
+  // Function to clear the entire cart
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  // Function to calculate the total price of items in the cart
+  const calculateTotalPrice = () => {
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  };
+
+  // Function to increment the quantity of an item in the cart
+  const incrementQuantity = (productId) => {
+    setCartItems((prevCartItems) =>
+      prevCartItems.map((item) =>
+        item.id === productId
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    );
+  };
+
+  // Function to decrement the quantity of an item in the cart
+  const decrementQuantity = (productId) => {
+    setCartItems((prevCartItems) =>
+      prevCartItems.map((item) =>
+        item.id === productId && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      )
+    );
+  };
+
+  // Load cart items from local storage when the component initializes
+  useEffect(() => {
+    const storedCartItems = localStorage.getItem("cartItems");
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
+    }
+  }, []); // Make sure to include an empty dependency array to run this effect only once when the component mounts.
+
+  // Function to save cart items to local storage when the cart changes
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  // Calculate the total nutrition based on cart items
+  const totalNutrition = calculateTotalNutrition(cartItems);
+
+  // Create the value object to provide to the context
+  const value = {
+    cartItems,
+    addToCart,
+    removeFromCart,
+    clearCart,
+    calculateTotalPrice,
+    incrementQuantity,
+    decrementQuantity,
+    totalNutrition, // Include totalNutrition in the value object
+  };
+
+  return (
+    <CartContext.Provider value={value}>
+      {children}
+    </CartContext.Provider>
+  );
 }
